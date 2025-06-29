3c94213e25903fc16673f62364b35d83ded0929c
Eduar <eduarnossa315@gmail.com>
2025-06-29 11:14:52 -0500

    Eliminar

--- clientes/src/main/java/com/eduar/clientes/repository/ClienteRepository.java
+++
@@ -1,8 +0,0 @@
-package com.eduar.clientes.repository;
-
-import com.eduar.clientes.model.Cliente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClienteRepository extends JpaRepository<Cliente, String> {
-
-}
